c3a47a9c62ea50f3e2ce1d5a3a492955a881331c
Alex <alex.raizman@hotmail.com>
2012-06-11 20:25:29 -0700

    Initial commit.
    Adding first draft of d3.cedr.js extending d3.layout and d3.svg for CEDR
    events time plot.
    Using examples from StreamInsight Hitchhiker's Guide as examples for
    visualization.
    Disclaimer: my first attempt at JavaScript code after couple of weeks of
    immersion. :)

---
+++ d3.cedr.js
@@ -0,0 +1,344 @@
+﻿// Extending d3.layout by adding cedr layout for drawing cedr events.
+// For now just supporting Point and Interval.
+d3.layout.cedr = function() {
+  var scale = 10,
+      minor = 0,
+      extent,
+      vs = function(x) { return x.vs },
+      ve = function(x) { return x.ve },
+      y = function(x, i) { return x.y || i },
+      d = function(x) { return x.d },
+      title = function(x, i) { return "e" + i };
+
+  function computeHExtent(data, paddingding) {
+    if(data.length === 0) {
+      return [0, 0];
+    } else {
+      var vsExtent = d3.extent(data.map(vs));
+      var veExtent = d3.extent(data.map(ve));
+      var tmin = Math.min(vsExtent[0], veExtent[0]) - paddingding;
+      var tmax = Math.max(vsExtent[1], veExtent[1]) + paddingding;
+      if(typeof extent === 'undefined') {
+        return [tmin, tmax];
+      } else {
+        return [Math.min(tmin, extent[0]), Math.max(tmax, extent[1])];
+      }
+    }
+  }
+
+  function computeVExtent(data) {
+    return data.length === 0 ? [0, 0] : d3.extent(data.map(y));
+  }
+
+  function cedr(data, winAllign) {
+    // Separate windows and events.
+    var windows = data.filter(function(x) { return x.kind === 'window'; });
+    var events = data.filter(function(x) { return x.kind !== 'window'; });
+    
+    // compute horizontal (time) extent
+    var winExtent = computeHExtent(windows, 0); // paddingding is 0 to draw windows all the way to the sides.
+    var eventExtent = computeHExtent(events, 1); // events however are paddingded by 1 scale unit.
+    var hExtent;
+    if(windows.length === 0) {
+      hExtent = eventExtent;
+    } else if(events.length === 0) {
+      hExtent = winExtent;
+    } else {
+      hExtent = [Math.min(winExtent[0], eventExtent[0]), Math.max(winExtent[1], eventExtent[1])];
+    }
+
+    // compute vertical extent and adjustment values for windows and events.
+    var winY = computeVExtent(windows);
+    var eventY = computeVExtent(events);
+    var winAdjust = 0,
+        eventAdjust = 0,
+        winDy = winY[1] - winY[0], // could be 0 if no windows
+        eventDy = eventY[1] - eventY[0]; // could be 0 if no events
+    if(winDy !== 0) winDy++; // add paddingding if there are windows
+    if(eventDy !== 0) eventDy++; // add paddingding if there are events
+    if(winAllign === 'top') {
+      winAdjust = eventDy + 1 - winY[0]; // move min to 1 above the events
+      eventAdjust = 1 - eventY[0]; // move min to 1
+    } else {
+      winAdjust = 1 - winY[0]; // move min to 1
+      eventAdjust = winDy + 1 - eventY[0]; // move min to 1 above the windows
+    }
+    var vExtent = [0, winDy + eventDy + 1];
+
+    // compute and apply scaling
+    var h = (vExtent[1] - vExtent[0] + 1) * scale * minor;
+    var w = (hExtent[1] - hExtent[0] + 1) * scale * minor;
+    var yScale = d3.scale.linear().domain(vExtent).range([-h, 0]); // -h..0 to account for screen Y-axis translation
+    var xScale = d3.scale.linear().domain(hExtent).range([0, w]);
+
+    return {
+      xAxis: d3.svg.axis()
+          .scale(xScale)
+          .orient('bottom')
+          .ticks(hExtent[1] - hExtent[0])
+          .tickSubdivide(minor)
+          .tickSize(-h, -h, 0),
+      yAxis : d3.svg.axis()
+          .scale(yScale)
+          .orient('left')
+          .ticks(vExtent[1] - vExtent[0])
+          .tickSubdivide(minor)
+          .tickSize(-w, -w, 0)
+          .tickFormat(''),
+      w: w,
+      h: h,
+      data: data.map(function(x, i) {
+        var vsVal = vs(x, i);
+        var veVal = ve(x, i);
+        var yVal = y(x, i) + (x.kind === 'window' ? winAdjust : eventAdjust);
+        var result = { kind: x.kind, vs: xScale(vsVal), y: -h - yScale(yVal), d: d(x, i), title: title(x, i) };
+        if(veVal === vsVal) {
+          // it's a point
+          if(typeof(x.extend === 'number')) {
+            result.extend = xScale(x.extend - 0.5);
+          }
+          result.ve =  xScale(vsVal + 0.5);
+        } else {
+          // it's an interval
+          if(typeof(x.extend === 'number')) {
+            result.extend = xScale(x.extend);
+          }
+          result.ve =  xScale(veVal);
+        }
+        if(x.kind === 'window') {
+          result.yExtend = yScale(winAllign === 'top' ? vExtent[1] : vExtent[0]);
+        }
+
+        return result;
+      })
+    };
+  };
+
+  cedr.scale = function(_) {
+    if (!arguments.length) return scale;
+    scale = _;
+    return cedr;
+  };
+
+  cedr.minor = function(_) {
+    if (!arguments.length) return minor;
+    minor = _;
+    return cedr;
+  };
+
+  cedr.vs = function(_) {
+    if (!arguments.length) return vs;
+    vs = _;
+    return cedr;
+  };
+
+  cedr.ve = function(_) {
+    if (!arguments.length) return ve;
+    ve = _;
+    return cedr;
+  };
+
+  cedr.y = function(_) {
+    if (!arguments.length) return y;
+    y = _;
+    return cedr;
+  };
+
+  cedr.d = function(_) {
+    if (!arguments.length) return d;
+    d = _;
+    return cedr;
+  };
+
+  cedr.title = function(_) {
+    if (!arguments.length) return title;
+    title = _;
+    return cedr;
+  };
+
+  cedr.extent = function(_) {
+    if (!arguments.length) return extent;
+    extent = _;
+    return cedr;
+  };
+
+  return cedr;
+}
+
+// Extend d3.svg with timePlot which utilizes d3.layout.cedr layout to draw
+// the timePlot of the cedr events.
+d3.svg.timePlot = function() {
+  // default color for events (blueish)
+  var color = function() { return "#1f77b4" };
+
+  function drawWindow(g, e, w) {
+    // draw the window line fragment...
+    g.append("svg:line").attr("class", "window")
+      .attr("x1", e.vs)
+      .attr("x2", e.ve)
+      .attr("y1", e.y)
+      .attr("y2", e.y);
+
+    // display title if any...
+    if(typeof e.title !== 'unknown') {
+      g.append("svg:text").attr("class", "event")
+        .attr("x", e.vs)
+        .attr("y", e.y - 4)
+        .attr("text-anchor", "left")
+        .text(e.title);
+    }
+
+    // vertical guide for window start and end if yExtend is specified.
+    if(typeof +e.yExtend === 'number')
+    {
+      // do not draw verical guides at the edges of the drawing
+      if(e.ve !== w && e.ve !== 0) {
+        g.append("svg:line")
+          .attr("x1", e.ve)
+          .attr("x2", e.ve)
+          .attr("y1", e.y)
+          .attr("y2", e.yExtend)
+          .attr("stroke", "black")
+          .attr("shape-rendering", "crispEdges")
+          .attr("stroke-width", 1)
+          .attr("stroke-dasharray", [1,5,1,5]);
+      }
+      if(e.vs !== w && e.vs !== 0) {
+        g.append("svg:line")
+          .attr("x1", e.vs)
+          .attr("x2", e.vs)
+          .attr("y1", e.y)
+          .attr("y2", e.yExtend)
+          .attr("stroke", "black")
+          .attr("shape-rendering", "crispEdges")
+          .attr("stroke-width", 1)
+          .attr("stroke-dasharray", [1,5,1,5]);
+      }
+    }
+  };
+
+  function drawEvent(g, e) {
+    // draw the event line fragment...
+    g.append("svg:line").attr("class", "event")
+      .attr("x1", e.vs)
+      .attr("x2", e.ve)
+      .attr("y1", e.y)
+      .attr("y2", e.y)
+      .attr("stroke", color(e.d))
+      .attr("stroke-width", 2);
+
+    // starting circle...
+    g.append("svg:circle").attr("class", "event")
+      .attr("cx", e.vs)
+      .attr("cy", e.y)
+      .attr("r", 3)
+      .attr("stroke", color(e.d))
+      .attr("fill", color(e.d));
+
+    // dotted time extension showing the effect of alterlifetime if extend is specified...
+    if(typeof e.extend === 'number') {
+      g.append("svg:line").attr("class", "event")
+        .attr("x1", e.ve)
+        .attr("x2", e.ve + e.extend)
+        .attr("y1", e.y)
+        .attr("y2", e.y)
+        .attr("stroke", color(e.d))
+        .attr("stroke-width", 2)
+        .attr("stroke-dasharray", 2);
+    }
+
+    // and the title is specified.
+    if(typeof e.title !== 'unknown') {
+      g.append("svg:text").attr("class", "event")
+        .attr("x", e.vs)
+        .attr("y", e.y - 4)
+        .attr("text-anchor", "left")
+        .text(e.title);
+    }
+  };
+
+  function timePlot(g, cedr) {
+    // yAxis
+    g.append("g")
+      .attr("class", "axis")
+      .call(cedr.yAxis);
+    // xAxis
+    g.append("g")
+      .attr("class", "axis")
+      .call(cedr.xAxis);
+
+    // event and window data
+    cedr.data.forEach(function(e, i) {
+      if(e.kind === 'window') {
+        drawWindow(g, e, cedr.w);
+      } else if(e.kind === 'event') {
+        drawEvent(g, e);
+      }
+    });
+  }
+
+  timePlot.color = function(_) {
+    if (!arguments.length) return color;
+    color = _;
+    return timePlot;
+  };
+
+  return timePlot;
+}
+
+// common helper function to not clutter the html files.
+function sampleDrawing(computeTracks, padding) {
+  var colors = [
+    "#1f77b4", // blue-ish
+    "#d62728", // red-ish
+    "#9467bd", // magenta-ish
+    "#ff7f0e", // orange-ish
+    "#8c564b" // brown-ish
+    ];
+  padding = +padding || 20;
+
+  var plot = d3.svg.timePlot()
+    .color(function(x) { return colors[x]; });
+
+  var cedr = d3.layout.cedr()
+    .minor(3)
+    .scale(7.958)
+    .d(function(x) { return x.d - 1 })
+    .title(function(x) { return x.title });
+
+  var tracks = computeTracks(cedr);
+
+  var w = d3.max(tracks, function(x) { return x.w });
+  var h = d3.sum(tracks, function(x) { return x.h });
+  var svg = d3.select("body")
+    .append("svg:svg")
+      .attr("width", w + 2 * padding)
+      .attr("height", h + 2 * padding)
+    .append("g")
+      .attr("transform", "translate(" + padding + "," + padding + ")");
+
+  var heights = [];
+  tracks.reduce(function(h, x) { var s = h + x.h; heights.push(s); return s; }, 0);
+
+  for(var i = tracks.length - 1; i >= 0; i -= 1)
+  {
+    var t = svg.append("g")
+      .attr("class", "track")
+      .attr("width", w)
+      .attr("height", heights[i])
+      .attr("transform", "translate(" + 0 + "," + heights[i] + ")");
+
+    plot(t, tracks[i]);
+  }
+};
+
+// Introduce string formatting like in C# with {0}, {1}, etc...
+// Taken from http://stackoverflow.com/questions/610406/javascript-equivalent-to-printf-string-format
+if(typeof String.prototype.format !== 'function') {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) { 
+      return typeof args[number] !== 'undefined' ? args[number] : match;
+      });
+  };
+};
---
+++ data.js
@@ -0,0 +1,111 @@
+﻿var data = (function(f) {
+  var raw = [
+      { vs: '12:01', ve: '12:03', id: 1, license: 'JNB 7001', state: 'NY', make: 'Honda'    , model: 'CRV'    , vehicleType: 1, vehicleWeight:     0, toll:  7.0, tag: '123456789' },
+      { vs: '12:02', ve: '12:03', id: 1, license: 'YXZ 1001', state: 'NY', make: 'Toyota'   , model: 'Camry'  , vehicleType: 1, vehicleWeight:     0, toll:  4.0, tag: '' },
+      { vs: '12:02', ve: '12:04', id: 3, license: 'ABC 1004', state: 'CT', make: 'Ford'     , model: 'Taurus' , vehicleType: 1, vehicleWeight:     0, toll:  5.0, tag: '456789123' },
+      { vs: '12:03', ve: '12:07', id: 2, license: 'XYZ 1003', state: 'CT', make: 'Toyota'   , model: 'Corolla', vehicleType: 1, vehicleWeight:     0, toll:  4.0, tag: '' },
+      { vs: '12:03', ve: '12:08', id: 1, license: 'BNJ 1007', state: 'NY', make: 'Honda'    , model: 'CRV'    , vehicleType: 1, vehicleWeight:     0, toll:  5.0, tag: '789123456' },
+      { vs: '12:05', ve: '12:07', id: 2, license: 'CDE 1007', state: 'NJ', make: 'Toyota'   , model: '4x4'    , vehicleType: 1, vehicleWeight:     0, toll:  6.0, tag: '321987654' },
+      { vs: '12:06', ve: '12:09', id: 2, license: 'BAC 1005', state: 'NY', make: 'Toyota'   , model: 'Camry'  , vehicleType: 1, vehicleWeight:     0, toll:  5.5, tag: '567891234' },
+      { vs: '12:07', ve: '12:10', id: 1, license: 'ZYX 1002', state: 'NY', make: 'Honda'    , model: 'Accord' , vehicleType: 1, vehicleWeight:     0, toll:  6.0, tag: '234567891' },
+      { vs: '12:07', ve: '12:10', id: 2, license: 'ZXY 1001', state: 'PA', make: 'Toyota'   , model: 'Camry'  , vehicleType: 1, vehicleWeight:     0, toll:  4.0, tag: '987654321' },
+      { vs: '12:08', ve: '12:10', id: 3, license: 'CBA 1008', state: 'PA', make: 'Ford'     , model: 'Mustang', vehicleType: 1, vehicleWeight:     0, toll:  4.5, tag: '891234567' },
+      { vs: '12:09', ve: '12:11', id: 2, license: 'DCB 1004', state: 'NY', make: 'Volvo'    , model: 'S80'    , vehicleType: 1, vehicleWeight:     0, toll:  5.5, tag: '654321987' },
+      { vs: '12:09', ve: '12:16', id: 2, license: 'CDB 1003', state: 'PA', make: 'Volvo'    , model: 'C30'    , vehicleType: 1, vehicleWeight:     0, toll:  5.0, tag: '765432198' },
+      { vs: '12:09', ve: '12:10', id: 3, license: 'YZX 1009', state: 'NY', make: 'Volvo'    , model: 'V70'    , vehicleType: 1, vehicleWeight:     0, toll:  4.5, tag: '912345678' },
+      { vs: '12:10', ve: '12:12', id: 3, license: 'BCD 1002', state: 'NY', make: 'Toyota'   , model: 'Rav4'   , vehicleType: 1, vehicleWeight:     0, toll:  5.5, tag: '876543219' },
+      { vs: '12:10', ve: '12:14', id: 1, license: 'CBD 1005', state: 'NY', make: 'Toyota'   , model: 'Camry'  , vehicleType: 1, vehicleWeight:     0, toll:  4.0, tag: '543219876' },
+      { vs: '12:11', ve: '12:13', id: 1, license: 'NJB 1006', state: 'CT', make: 'Ford'     , model: 'Focus'  , vehicleType: 1, vehicleWeight:     0, toll:  4.5, tag: '678912345' },
+      { vs: '12:12', ve: '12:15', id: 3, license: 'PAC 1209', state: 'NJ', make: 'Chevy'    , model: 'Malibu' , vehicleType: 1, vehicleWeight:     0, toll:  6.0, tag: '219876543' },
+      { vs: '12:15', ve: '12:22', id: 2, license: 'BAC 1005', state: 'PA', make: 'Peterbilt', model: '389'    , vehicleType: 2, vehicleWeight: 2.675, toll: 15.5, tag: '567891234' },
+      { vs: '12:15', ve: '12:18', id: 3, license: 'EDC 3109', state: 'NJ', make: 'Ford'     , model: 'Focus'  , vehicleType: 1, vehicleWeight:     0, toll:  4.0, tag: '198765432' },
+      { vs: '12:18', ve: '12:20', id: 2, license: 'DEC 1008', state: 'NY', make: 'Toyota'   , model: 'Corolla', vehicleType: 1, vehicleWeight:     0, toll:  4.0, tag: '' },
+      { vs: '12:20', ve: '12:22', id: 1, license: 'DBC 1006', state: 'NY', make: 'Honda'    , model: 'Civic'  , vehicleType: 1, vehicleWeight:     0, toll:  5.0, tag: '' },
+      { vs: '12:20', ve: '12:23', id: 2, license: 'APC 2019', state: 'NJ', make: 'Honda'    , model: 'Civic'  , vehicleType: 1, vehicleWeight:     0, toll:  4.0, tag: '345678912' },
+      { vs: '12:22', ve: '12:25', id: 1, license: 'EDC 1019', state: 'NJ', make: 'Honda'    , model: 'Accord' , vehicleType: 1, vehicleWeight:     0, toll:  4.0, tag: '432198765' }
+  ];
+
+  return function(selector) {
+    if(typeof(selector) === 'function') {
+      return raw.map(selector);
+    } else {
+      return raw;
+    }
+  };
+}());
+
+var sliding = function(events) {
+  var prev = 0,
+      cur,
+      y = 1;
+      result = [],
+	    sync = events
+        .map(function(d) { return d.vs })
+        .concat(events
+          .map(function(d) { return d.ve }))
+	      .sort(function(a, b) { return a-b });
+
+  for(var i = 0, imax = sync.length; i < imax; i += 1)
+  {
+    cur = sync[i];
+    if(cur != prev) {
+	    result.push(
+        { kind: 'window', hExtend: 0, vs: prev, ve: cur, y: y,
+          toString: function() { return 'vs: ' + this.vs + ', ve: ' + this.ve + ', d: ' + this.d; }
+        });
+	  prev = cur;
+	  y = 3 - y;
+	  }
+  }
+  
+  return result;
+};
+
+var hopping = function(max, win, hop) {
+  max = +max;
+  win = +win;
+  hop = +hop || +win;
+  var result = [],
+      vs,
+	    ve,
+	    y = 0,
+	    loop = win <= hop ? 2 : Math.floor(win/hop) + 2;
+
+  for(var i = -Math.max(win, hop); i <= max; i = i + hop) {
+    vs = Math.max(0, i);
+    ve = Math.min(max, i + win);
+    if(ve > 0 && vs < max) {
+      result.push(
+        { kind: 'window', hExtend: 0, vs: vs, ve: ve, y: y % loop + 1,
+          toString: function() { return 'vs: ' + this.vs + ', ve: ' + this.ve + ', y: ' + this.y; }
+        });
+	    y = y + 1;
+    }
+  }
+
+  return result;
+};
+
+var rawEvents = data(function(d, i) {
+  var ret = Object.create(d);
+  ret.kind = 'event';
+  ret.vs = +d.vs.split(':')[1];
+  ret.ve = +d.ve.split(':')[1];
+  ret.d = d.id;
+  ret.title = "e" + (i+1);
+  ret.extend = 0;
+  ret.toString = function() { return 'vs: ' + this.vs + ', ve: ' + this.ve + ', d: ' + this.d + ', title: ' + this.title; };
+	return ret;
+});
+
+var rawPoints = data(function(d, i) {
+  var ret = Object.create(d);
+  ret.kind = 'event';
+  ret.vs = +d.vs.split(':')[1];
+  ret.ve = +d.ve.split(':')[1];
+  ret.d = d.id;
+  ret.title = "e" + (i+1);
+  ret.extend = 0;
+  ret.toString = function() { return 'vs: ' + this.vs + ', ve: ' + this.ve + ', d: ' + this.d + ', title: ' + this.title; };
+  return ret;
+});
